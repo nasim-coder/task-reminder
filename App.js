a19498c47acb65a78bb93c4cd3a01657d796b4a1
N451M4K <khannasimahmad@gmail.com>
2023-05-27 08:19:24 +0530

    added icons in app

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 import Task from './components/Task';
 export default function App() {
+
+  const [tasks, setTasks] = useState([]);
+
   return (
     <View style={styles.container}>
       <View style = {styles.taskWrapper}>
       <Text style={styles.title}>Todays Tasks :</Text>
         <View style={styles.items}>
-          <Task />
+          <Task task={{taskTitle:'hello', description:'lorem ipsum', time: Date.now()}} />
+          <Task task={{taskTitle:'hello', description:'lorem ipsum', time: Date.now()}} />
         </View>
       </View>
     </View>
@@ -26,11 +31,11 @@ const styles = StyleSheet.create({
   },
 
   items: {
-
+    marginTop: 10,
   },
 
   taskWrapper: {
     paddingTop: 40,
-    marginHorizontal: 40,
+    marginHorizontal: 20,
   },
 });
--- components/Task.js
+++ components/Task.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const Task = () => {
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+const Task = (props) => {
     return (
-        <View>
-            <Text>This is a task</Text>
+        <View style={styles.task}>
+            <Text style={styles.taskItem}>{props.task.taskTitle}</Text>
+            <Text style={styles.taskItem}>{props.task.description}</Text>
+            <Text>{props.task.time}</Text>
+            <TouchableOpacity style={styles.taskItem}>
+                <AntDesign name="edit" size={24} color="blue" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.taskItem}>
+            <AntDesign name="delete" size={24} color="red" />
+            </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    task: {
+        margin: 5,
+        flexDirection: 'row',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+    },
+    taskItem: {
+        marginHorizontal:5,
+    }
 })
 
 export default Task;
