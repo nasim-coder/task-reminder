1a307227b8ba4f1c7955200ca7ca9d9e666e61b7
N451M4K <khannasimahmad@gmail.com>
2023-07-16 13:20:26 +0530

    created coponent Weekly to show only weekly tasks

---
+++ components/Weekly.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+
+import Home from './Home';
+
+function Weekly() {
+  return <Home frequencyData="weekly" />;
+}
+
+export default Weekly;
