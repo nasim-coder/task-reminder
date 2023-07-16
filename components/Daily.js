ac79281f82ed04de33d1b2cf7e83c34d41c1c76a
N451M4K <khannasimahmad@gmail.com>
2023-07-16 13:16:53 +0530

    created Daily Component and its content

---
+++ components/Daily.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+import Home from './Home';
+
+function Daily() {
+  return <Home frequencyData="daily" />;
+}
+
+export default Daily;
