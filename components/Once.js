f186304750f4cc224a7c2002234312cdae45ebb7
N451M4K <khannasimahmad@gmail.com>
2023-07-16 13:18:17 +0530

    created Once component

---
+++ components/Once.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+import Home from './Home';
+
+function Once() {
+  return <Home frequencyData="once" />;
+}
+
+export default Once;
