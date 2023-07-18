b0019cc25de0f61331a5395e2090f2eba7f92e23
N451M4K <khannasimahmad@gmail.com>
2023-07-18 14:29:13 +0530

    made changes to show date or day below time

--- components/Task.js
+++ components/Task.js
@@ -8,19 +8,34 @@ import { View, Text, StyleSheet } from 'react-native';
 function Task(props) {
   // eslint-disable-next-line react/destructuring-assignment
   const time = new Date(props?.task?.time);
+  const { selectedDays, frequency } = props.task;
+  // console.log('frequency', frequency);
+  // console.log('selectedDays', typeof props.task.selectedDays);
+  let day;
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  if (frequency === 'once') {
+    const date = new Date(props.task.date);
+    day = `${date.getDate()}-${months[date.getMonth()]}-${date.getFullYear()}`;
+  } else if (frequency === 'weekly') {
+    day = days[selectedDays - 1];
+  }
+  // console.log(day);
   return (
     <View style={styles.taskContainer}>
       <Text style={styles.task} numberOfLines={3}>
         {props.task.task}
       </Text>
-      <Text style={styles.time}>
-        {time.toLocaleString('en-US', {
-          hour: 'numeric',
-          minute: 'numeric',
-          hour12: true,
-        })}
-
-      </Text>
+      <View style={styles.timeContainer}>
+        <Text style={styles.time}>
+          {time.toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+          })}
+        </Text>
+        {day && <Text style={styles.day}>{day}</Text>}
+      </View>
     </View>
   );
 }
@@ -44,10 +59,17 @@ const styles = StyleSheet.create({
     height: 40,
     textAlignVertical: 'center',
   },
-
+  timeContainer: {
+    flexDirection: 'column', // Change to column
+    alignItems: 'flex-end', // Align time and day to the right
+  },
   time: {
-    fontSize: 14,
+    fontSize: 13,
     fontWeight: 'bold',
+    color: '#9C1D9E',
+  },
+  day: {
+    fontSize: 13,
     color: '#9C1D9E',
   },
 });
